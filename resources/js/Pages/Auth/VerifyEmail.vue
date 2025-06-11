ea6766af43c6a80ff250c14d65c7db0e0934707d
Pavel <pavelbalandin@gmail.com>
2025-06-11 18:32:01 +0300

    update: Simplify the Russian translation for "Resend Verification Email" and update the VerifyEmail.vue component to use internationalization. Ensure consistency in the code formatting and improve readability by adjusting spacing and structure.

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { computed } from 'vue';
+import {computed} from 'vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import {Head, Link, useForm} from '@inertiajs/vue3';
 
 const props = defineProps({
     status: {
@@ -23,37 +23,36 @@ const verificationLinkSent = computed(
 
 <template>
     <GuestLayout>
-        <Head title="Email Verification" />
+        <Head title="Email Verification"/>
 
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            Thanks for signing up! Before getting started, could you verify your
-            email address by clicking on the link we just emailed to you? If you
-            didn't receive the email, we will gladly send you another.
+            {{
+                $t("Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.")
+            }}
         </div>
 
-        <div
-            class="mb-4 text-sm font-medium text-green-600 dark:text-green-400"
-            v-if="verificationLinkSent"
-        >
-            A new verification link has been sent to the email address you
-            provided during registration.
+        <div class="mb-4 text-sm font-medium text-green-600 dark:text-green-400"
+             v-if="verificationLinkSent">
+            {{
+                $t("A new verification link has been sent to the email address you provided during registration.")
+            }}
         </div>
 
         <form @submit.prevent="submit">
             <div class="mt-4 flex items-center justify-between">
                 <PrimaryButton
                     :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Resend Verification Email
+                    :disabled="form.processing">
+                    {{ $t('Resend Verification Email') }}
                 </PrimaryButton>
 
                 <Link
                     :href="route('logout')"
                     method="post"
                     as="button"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
-                    >Log Out</Link
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
+                    {{ $t('Log Out') }}
+                </Link
                 >
             </div>
         </form>
